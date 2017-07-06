dbf2d37d9ebe3f8921c8f61a7e4bf246962498a7
teddddd <ted@mixpanel.com>
2017-07-05 18:58:58 -0700

    example: button to send parent-child msg

--- example/child.js
+++ example/child.js
@@ -1,3 +1,7 @@
 import Framesg from '../lib';
 
+const child = new Framesg(window.parent, 'example-app', {
+  ping: msg => console.log(`message from parent: ${msg}`),
+});
+
 console.log(`Hello from child`);
--- example/parent.js
+++ example/parent.js
@@ -5,4 +5,9 @@ const child = new Framesg(childEl.contentWindow, 'example-app', {
   ping: msg => console.log(`message from child: ${msg}`),
 });
 
+const childMsgButton = document.querySelector('#child-msg-button');
+childMsgButton.addEventListener('click', () => {
+  child.send('ping', 'moo');
+});
+
 console.log(`Hello from parent`);
